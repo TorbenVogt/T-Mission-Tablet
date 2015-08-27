8e029a88863aa4277c04c9055c28f69f874e3971
matttx1111 <mathias.hoffmann90@gmail.com>
2015-08-27 12:47:53 +0200

    ViewModel Locator added
    Added a vml is working for the mainviewmodel and the main page as view

---
+++ HelloWorld/HelloWorld/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class MainViewModel : ViewModelBase 
+    {
+       private int dummyValue = 3;
+        
+       public int DummyValue
+        {
+            get
+            {
+                return dummyValue;
+            }
+            set
+            {
+                dummyValue = value;
+                RaisePropertyChanged();
+            }
+        }               
+    }
+}
---
+++ HelloWorld/HelloWorld/ViewModelLocator.cs
@@ -0,0 +1,40 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Command;
+
+using Microsoft.Practices.ServiceLocation;
+
+
+namespace HelloWorld
+{
+    
+
+    public class ViewModelLocator
+    {
+        
+
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+            
+            if (ViewModelBase.IsInDesignModeStatic)
+            {
+              //  SimpleIoc.Default.Register<IDataService, Design.DesignDataService>();
+            }
+            else
+            {
+                //SimpleIoc.Default.Register<IDataService, DataService>();
+            }
+
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+        
+        public ViewModelBase MainViewModel
+        {
+            get { return SimpleIoc.Default.GetInstance<MainViewModel>(); }
+        }
+        
+        
+        
+    }
+}
