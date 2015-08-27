3c2b7030af0b70edd1a9378f041541ea023fb264
TorbenVogt <Vogt91@gmail.com>
2015-08-27 12:45:55 +0200

    XML denied.
    Denied

--- HelloWorld/HelloWorld/MainPage.xaml.cs
+++ HelloWorld/HelloWorld/MainPage.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using System.Xml.Linq;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,11 +28,21 @@ namespace HelloWorld
         public MainPage()
         {
             this.InitializeComponent();
+
+          Task task = new Task(ReadOurXML);
+          task.Start();
+           
         }
 
         private void inputButton_Click(object sender, RoutedEventArgs e)
         {
             greetingOutput.Text = "Hello mr. Scholar and gentleman " + nameInput.Text + "!" + " How are you?";
+        }
+
+        public static async void ReadOurXML()
+        {
+            XElement booksFromFile = XElement.Load(@"C:\AppData\dummy.xml");
+  
         }
     }
 }
---
+++ HelloWorld/HelloWorld/XMLStreamReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+using System.Xml.Linq;
+using Windows.Storage;
+
+
+namespace HelloWorld
+{
+   public class XMLStreamReader
+    {
+       public static string ReadXMLfile()
+       {
+
+           
+
+           
+
+
+            return "bob";
+
+       }
+    }
+}
