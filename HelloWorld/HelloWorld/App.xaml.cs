60da187b3dccafb0686a4ae3562d3cbb168f7d43
matttx1111 <mathias.hoffmann90@gmail.com>
2015-09-10 12:47:53 +0200

    CHanges to reflect name and started loadoverview
    Load overview started, old main changed to this

--- HelloWorld/HelloWorld/App.xaml.cs
+++ HelloWorld/HelloWorld/App.xaml.cs
@@ -69,7 +69,7 @@ namespace TMissionMobile
                 // When the navigation stack isn't restored navigate to the first page,
                 // configuring the new page by passing required information as a navigation
                 // parameter
-                rootFrame.Navigate(typeof(Views.MainPage), e.Arguments);
+                rootFrame.Navigate(typeof(Views.LoadOverviewView), e.Arguments);
             }
             // Ensure the current window is active
             Window.Current.Activate();
--- HelloWorld/HelloWorld/Views/LoadOverviewView.xaml.cs
+++ HelloWorld/HelloWorld/Views/LoadOverviewView.xaml.cs
@@ -8,9 +8,9 @@ namespace TMissionMobile.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class LoadOverviewView : Page
     {
-        public MainPage()
+        public LoadOverviewView()
         {
             InitializeComponent();
 
