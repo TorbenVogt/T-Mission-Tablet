3064f35c4e0c0a041ec140993f7909201b7b0e85
TorbenVogt <Vogt91@gmail.com>
2015-09-21 10:40:25 +0200

    Added BMP. Load bmp from viewmodel

---
+++ HelloWorld/HelloWorld/Viewmodels/LoadSpecDialogViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Shapes;
+using GalaSoft.MvvmLight;
+
+namespace TMissionMobile.Viewmodels
+{
+    class LoadSpecDialogViewModel : ViewModelBase
+    {
+        public string DisplayedImage
+        {
+            get { return "ms-appx:///Assets/homer-simpson.bmp"; }
+        }
+    }
+}
--- HelloWorld/HelloWorld/Views/LoadSpecDialog.xaml.cs
+++ HelloWorld/HelloWorld/Views/LoadSpecDialog.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Content Dialog item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
@@ -22,7 +23,6 @@ namespace TMissionMobile.Views
         public LoadSpecDialog()
         {
             this.InitializeComponent();
-
             GetTypeOfLoad();
         }
 
